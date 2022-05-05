41efe8e0fff8e1b85031f15f49e41bb475a48249
Erika Stadnik <erifox12@gmail.com>
2022-05-05 22:52:32 +0100

    Footer and Header update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   
   return (
     <div>
-      <div className="py-4 bg-fill text-light_text max-w-10xl mx-auto px-10">
+      <div className="py-2 bg-fill text-light_text max-w-10xl mx-auto px-10">
           <p className="font-mono text-sm">mirr's app, Erika Stadnik 2022</p> 
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
   
   return (
     <div className="max-w-10xl mx-auto px-0">
-      <div className="border-b-2 border-fill divide-slate-800 py-7 px-10 bg-white items-center flex justify-between content-center ">
-          <Link to="/mirrr/" className="text-4xl font-bold font-sans text-text">mirr'r</Link> 
+      <div className="border-b-2 border-fill divide-slate-800 py-3 px-10 bg-white items-center flex justify-between content-center ">
+          <Link to="/mirrr/" className="text-2xl font-bold font-sans text-text">mirr'r</Link> 
       </div>
     </div>
   )
