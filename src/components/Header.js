2d3b819f482687b72531283fb89252cb7fe13513
Erika Stadnik <erifox12@gmail.com>
2022-05-09 16:25:46 +0100

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom"
 import React from 'react'
+import store from '../store'
+import { uploadData } from '../reducers/dataReducer'
 
 const Header = () => {
 
- 
+  const handleClear = () => {
+    store.dispatch(uploadData(null))
+    console.log(store.getState().data)
+
+  }
   
   return (
     <div className="max-w-10xl mx-auto px-0">
       <div className="border-b-2 border-fill divide-slate-800 py-3 px-10 bg-white items-center flex justify-between content-center ">
-          <Link to="/mirrr/" className="text-2xl font-bold font-sans text-text">mirr'r</Link> 
+          <Link to="/mirrr/" onClick={handleClear} className="text-2xl font-bold font-sans text-text">mirr'r</Link> 
       </div>
     </div>
   )
